a8cefd23095591e4b7477cfffbd9b2737b4db87d
Ozan Erel <timurerel@gmail.com>
2018-11-23 10:27:54 +0100

    [add] faq links

---
+++ test_46_xs4all_home_topxservicevragen.js
@@ -0,0 +1,6 @@
+var container, content;
+
+container = document.querySelector('.py-4 div:nth-of-type(3)');
+content = '<h4>Meteen antwoord <br class="d-none d-lg-inline">op uw vraag</h4> <p class="mb-0 xs4all-faq"> <a data-ats-id="homepage-button-search" class="kpi-wifi" href="https://www.xs4all.nl/wifi/">Informatie over WiFi</a> <i class="icon icon--chevron-right-light"></i> </p><p class="mb-0 xs4all-faq"> <a data-ats-id="homepage-button-search" class="kpi-email" href="https://www.xs4all.nl/service/diensten/email/installeren/algemene-instellingen.htm">Stel zelf uw e-mail in</a> <i class="icon icon--chevron-right-light"></i> </p><p class="mb-0 xs4all-faq"> <a data-ats-id="homepage-button-search" class="kpi-zenderpakketten" href="https://www.xs4all.nl/pakketten/internet-en-tv/">Welke extra zenderpakketten zijn er?</a> <i class="icon icon--chevron-right-light"></i> </p><p class="mb-0 xs4all-faq"> <a data-ats-id="homepage-button-search" class="kpi-service" href="https://www.xs4all.nl/service/waarmee-kunnen-we-u-helpen.htm">Of vind hier het antwoord op uw vraag</a> <i class="icon icon--chevron-right-light"></i> </p>';
+
+container.innerHTML = content;
